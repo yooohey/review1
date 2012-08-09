58165064f39542a907ea0179059b845f0454ffb8
ict <ict@R1-17.local>
2012-08-09 20:22:49 +0900

    add class Person

---
+++ src/Person.java
@@ -0,0 +1,45 @@
+public class Person {
+	static String uji = new String("氏");
+	static String na = new String("名");
+	public String firstname = new String("");
+	public String lastname = new String("");
+
+	public String getfirstname(){
+		return this.firstname;
+	}
+
+	public void setfirstname(String name){
+		this.firstname = name;
+	}
+
+	public String getlastname(){
+		return this.lastname;
+	}
+
+	public void setlastname(String name){
+		this.lastname = name;
+	}
+
+	public void showname(){
+		System.out.println("氏:"+this.firstname+" "+"名:"+this.lastname);
+	}
+
+	public static void main(String[] args){
+		String firstname1 = new String("渡辺");
+		String lastname1 = new String("高志");
+		String firstname2 = new String("和田");
+		String lastname2 = new String("充史");
+
+		this.setfirstname("firstname1");
+		this.setlastname("lastname1");
+		this.showname();
+
+		this.setfirstname("firstname2");
+		this.setlastname("lastname2");
+		this.showname();
+
+		this.setfirstname("firstname1");
+		this.setlastname("lastname1");
+		this.showname();
+	}
+}
